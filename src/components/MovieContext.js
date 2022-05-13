13322bf8e9ad7638eec874dc7c7c3bb0495c0443
Poorva Lalawat <poorvalalawat@Poorva-Lalawat-I157s-MacBook-Pro.local>
2022-05-13 13:08:36 +0530

    Initial commit

---
+++ src/components/MovieContext.js
@@ -0,0 +1,33 @@
+import React, {useState, createContext} from 'react';
+export const MovieContext= createContext();
+
+export const MovieProvider = (props) =>
+{
+    const [movies, setMovies]= useState ([
+        {
+            name: 'Harry Potter',
+            price: '$10',
+            id:23124
+        },
+    
+        {
+            name: 'D3',
+            price: '$40',
+            id:23128
+    
+        },
+        {
+            name: 'Harry Potter 7',
+            price: '$10',
+            id:23129
+        }
+    ]);
+
+    return (
+
+        <MovieContext.Provider values={[movies,setMovies]}>
+            {props.children}
+        </MovieContext.Provider>
+    );
+
+}; 
